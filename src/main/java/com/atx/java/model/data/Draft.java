9126f9f21d787a70dd8372b10cbebd416da37564
Pritam <pritam.sarkar@medlife.com>
2020-07-04 17:54:40 +0530

    Basic common models: draft, order

---
+++ src/main/java/com/atx/java/model/data/Draft.java
@@ -0,0 +1,9 @@
+package com.atx.java.model.data;
+
+import lombok.Data;
+
+@Data
+public class Draft extends Signal {
+
+    private double quantity;
+}
---
+++ src/main/java/com/atx/java/model/data/Order.java
@@ -0,0 +1,9 @@
+package com.atx.java.model.data;
+
+import lombok.Data;
+
+@Data
+public class Order {
+
+    private String draftId;
+}
